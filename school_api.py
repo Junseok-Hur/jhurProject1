2691fef10c6480db2577a76dc3419fd545fef675
veilside86 <gjwnstjr100@gmail.com>
2021-02-07 19:11:57 -0500

    fix some formatting errors for flake8

--- school_api.py
+++ school_api.py
@@ -30,7 +30,9 @@ def save_data(data, filename='SchoolData.txt'):
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2017.student.size,2018.student.size,school.city,school.state,school.name"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=" \
+          "2,3&fields=id,2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_" \
+          "count_over_poverty_line,2017.student.size,2018.student.size,school.city,school.state,school.name"
 
     all_data = get_data(url)
     for school_data in all_data:
