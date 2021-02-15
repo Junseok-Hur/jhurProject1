c0bcddfa11f325a27901a5d37ce530e2b8d2ea48
veilside86 <gjwnstjr100@gmail.com>
2021-02-14 23:29:43 -0500

    DATA UPLOAD

--- README.md
+++ README.md
@@ -3,10 +3,22 @@
 
 2. Any install and run directions I need beyond the basics.
 - requests
-- Confidential API Key (differs by users) 
+- Confidential API Key (differs by users)
+- sqlite3
+- tuple
 
 3. A brief description of what your project does
 - API Data is received from the site using API Key and inserted as a text file.
 
-4. A brief description of what is missing from the project (if anything)
-- N/A
+4. a very brief discussion of your database layout and the table(s) you used
+
+5. A brief description of what is missing from the project (if anything)
+- Can not insert the date into table
+{
+  "error": {
+    "code": "OVER_RATE_LIMIT",
+    "message": "You have exceeded your rate limit. Try again later or contact us at https://api.data.gov:443/contact/ for assistance"
+  }
+}
+  
+sqlite3.ProgrammingError: Incorrect number of bindings supplied. The current statement uses 8, and there are 3203 supplied.
--- school_api.py
+++ school_api.py
@@ -1,5 +1,49 @@
 import requests
 import secrets
+import sqlite3
+from typing import Tuple
+
+
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)   # connect to existing DB or create new one
+    cursor = db_connection.cursor()     # get ready to read/write data
+    return db_connection, cursor
+
+
+def setup_db(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS schools(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    state TEXT NOT NULL,
+    city TEXT NOT NULL,
+    '2018.student.size' INTEGER DEFAULT NULL,
+    '2017.student.size' INTEGER DEFAULT NULL,
+    '2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line' INTEGER DEFAULT NULL,
+    '2016.repayment.3_yr_repayment.overall' INTEGER DEFAULT NULL
+    );''')
+    print('table created')
+
+
+def close_db(connection: sqlite3.Connection):
+    connection.commit()     # make sure any changes get saved
+    connection.close()
+
+
+# def add_school(cursor: sqlite3.Cursor, id, name, state, city, STU_2018_N, STU_2017_N, CNTOVER150_3YR, RPY_3YR_N):
+#     cursor.execute(f'''INSERT INTO STUDENTS (id, name, state, city, STU_2018_N, STU_2017_N, CNTOVER150_3YR, RPY_3YR_N)
+#     VALUES({id}, {name}, {state}, {city}, {STU_2018_N}, {STU_2017_N}, {CNTOVER150_3YR}, {RPY_3YR_N})''')
+#
+
+
+def initial_schools(cursor: sqlite3.Cursor, data):
+    cursor.executemany("INSERT INTO schools(id, name, state, city, '2018.student.size', '2017.student.size', "
+                       "'2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line', "
+                       "'2016.repayment.3_yr_repayment.overall') VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
+
+
+def check_table(cursor: sqlite3.Cursor):
+    for row in cursor.fetchall():
+        print(row)
 
 
 def get_data(url: str):
@@ -14,6 +58,7 @@ def get_data(url: str):
             return []
         json_data = response.json()
         page_data = json_data["results"]
+        # initial_schools(cursor, page_data)
         final_data.extend(page_data)
         if len(page_data) < 20:
             entire_data = False
@@ -33,11 +78,17 @@ def main():
     url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=" \
           "2,3&fields=id,2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_" \
           "count_over_poverty_line,2017.student.size,2018.student.size,school.city,school.state,school.name"
-
+    conn, cursor = open_db("school_db.sqlite")
     all_data = get_data(url)
+    save_data(all_data)
+    dataframe = list()
+    dataframe.append(all_data)
     # for school_data in all_data:
     #    print(school_data)
-    save_data(all_data)
+    setup_db(cursor)
+    initial_schools(cursor, dataframe)
+    # check_table(cursor)
+    close_db(conn)
 
 
 if __name__ == '__main__':
