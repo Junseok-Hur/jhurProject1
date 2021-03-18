6eeb4d04815de2374b7482cbfc04a659ddd8c0f8
Junseok-Hur <j1hur@student.bridgew.edu>
2021-03-18 15:01:33 -0400

    So far refactor, update from db, choose excel work. I'm gonna do visual map data

--- school_api.py
+++ school_api.py
@@ -1,8 +1,29 @@
 import requests
+from PyQt5 import QtWidgets
 import secrets
 import sqlite3
 from typing import Tuple
 import pandas as pd
+import sys
+import school_api_gui
+import plotly.graph_objects as go
+
+
+def render_map():
+    fig = go.Figure(go.Scattergeo())
+    fig.update_geos(
+        visible=False, resolution=110, scope="usa",
+        showcountries=True, countrycolor="Black",
+        showsubunits=True, subunitcolor="Blue"
+    )
+    fig.update_layout(height=300, margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.show()
+
+
+def display_data():
+    app = QtWidgets.QApplication(sys.argv)  # sys.argv is the list of command line arguments
+    my_window = school_api_gui.Ui_MainWindow()
+    sys.exit(app.exec_())
 
 
 def save_excel_db(filename: str, conn):
@@ -18,6 +39,7 @@ def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
 
 
 def setup_db(cursor: sqlite3.Cursor):
+    cursor.execute("DROP TABLE IF EXISTS schools")
     cursor.execute('''CREATE TABLE IF NOT EXISTS schools(
     school_id INTEGER PRIMARY KEY,
     school_name TEXT NOT NULL,
@@ -26,7 +48,8 @@ def setup_db(cursor: sqlite3.Cursor):
     student_size_2018 INTEGER,
     student_size_2017 INTEGER,
     three_year_earnings_over_poverty INTEGER,
-    repayment_overall INTEGER
+    repayment_overall INTEGER,
+    repayment_cohort INTEGER
     );''')
     # print('table created')
 
@@ -41,22 +64,24 @@ def save_db(cursor, data):
         cursor.execute(
             """INSERT INTO schools(school_id, school_name, school_state,
             school_city, student_size_2018, student_size_2017,
-            three_year_earnings_over_poverty, repayment_overall) VALUES(?, ?, ?, ?, ?, ?, ?, ?)""",
+            three_year_earnings_over_poverty, repayment_overall, repayment_cohort) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)""",
             (school_data['id'], school_data['school.name'], school_data['school.state'], school_data['school.city'],
              school_data['2018.student.size'], school_data['2017.student.size'],
              school_data['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'],
-             school_data['2016.repayment.3_yr_repayment.overall']))
+             school_data['2016.repayment.3_yr_repayment.overall'],
+             school_data['2016.repayment.repayment_cohort.3_year_declining_balance']))
 
 
 def get_data():
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=" \
+          "2,3&_fields=id,school.name,school.state,school.city,2018.student.size,2017.student.size,2017." \
+          "earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall" \
+          ",2016.repayment.repayment_cohort.3_year_declining_balance"
     final_data = []
     entire_data = True
     page = 0
     while entire_data:
-        final_url = f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=" \
-                    f"2,3&_fields=id,school.name,school.state,school.city,2018.student.size,2017.student.size,2017." \
-                    f"earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment." \
-                    f"overall&api_key={secrets.api_key}&page={page}"
+        final_url = f"{url}&api_key={secrets.api_key}&page={page}"
         response = requests.get(final_url)
         if response.status_code != 200:
             print(response.text)
@@ -80,25 +105,20 @@ def save_data(data, filename='SchoolData.txt'):
 
 
 def main():
-    # max row 36383
     # url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=" \
     #       "2,3&_fields=id,school.name,school.state,school.city,2018.student.size,2017.student.size,2017." \
     #       "earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
-    excel_file = "state_M2019_dl.xlsx"
+    # excel_file = "state_M2019_dl.xlsx"
     conn, cursor = open_db("school_db.sqlite")
+    all_data = get_data()
+    save_data(all_data)
+    setup_db(cursor)
+    save_db(cursor, all_data)
+    # all_data.sort(key=get_key)
+    # save_excel_db(excel_file, conn)
     # conn, cursor = open_db("state_db.sqlite")
-    # open_excel(filename)
-    # dataframe = pd.read_excel(excel_file)
-    # dataframe_subset = dataframe[['area_title', 'occ_title', 'h_pct25', 'a_pct25', 'occ_code']]
-    # dataframe_subset.to_sql(name='states', con=conn, if_exists='append')
-    # all_data = get_data()
-    # save_data(all_data)
-    # setup_db(cursor)
-    # save_db(cursor, all_data)
-    save_excel_db(excel_file, conn)
+    display_data()
     close_db(conn)
-    # print(states_subset)
-    # print(all_data)
 
 
 if __name__ == '__main__':
