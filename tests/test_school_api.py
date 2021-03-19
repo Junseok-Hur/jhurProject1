066429b396797ce65ff87ce49528659b7ffe00a4
Junseok-Hur <j1hur@student.bridgew.edu>
2021-03-19 18:06:15 -0400

    Sprint3 pytest modified

--- tests/test_school_api.py
+++ tests/test_school_api.py
@@ -1,4 +1,21 @@
 import school_api as sa
+import openpyxl
+import pytest
+
+
+@pytest.fixture
+def get_db():
+    conn, cursor = sa.open_db("testdb.sqlite")
+    return conn, cursor
+
+@pytest.fixture
+def get_test_job_data():
+    test_dict = {'state_name': "TEST",
+                 'state_abbrev': 'ME',
+                 'occupation_title': "Student",
+                 'total_employment_in_field': 10000,
+                 "salary_lower25": 20000.65,
+                 'occupation_code': '11-0000'}
 
 
 def test_get_data():
@@ -13,7 +30,8 @@ def test_save_db():
     test_data = [{'school.name': 'Test University', '2018.student.size': 1000, '2017.student.size': 800,
                   'school.state': 'MA', 'id': 11001, 'school.city': 'Brockton',
                   '2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line': 456,
-                  '2016.repayment.3_yr_repayment.overall': 4004}]
+                  '2016.repayment.3_yr_repayment.overall': 4004,
+                  '2016.repayment.repayment_cohort.3_year_declining_balance': 0.21654843}]
     sa.save_db(cursor, test_data)
     sa.close_db(conn)
     # test data is saved - now lets see if it is there
@@ -29,25 +47,25 @@ def test_save_db():
     assert test_record[0] == 'Test University'
 
 
-def test_read_xlsx_properly():
-    excel_file = "./tests/state_M2019_dl.xls"
-    conn, cursor = sa.open_db("testdb.sqlite")
+def test_read_xlsx_properly(get_db):
+    excel_file = "state_M2019_dl.xlsx"
+    conn, cursor = get_db
     df = sa.pd.read_excel(excel_file)
-    df.to_sql(name='test_states', con=conn, if_exists='append', index=False)
+    df.to_sql(name='test_states', con=conn, if_exists='replace', index=False)
     cursor.execute('''SELECT test_states.area_title as "State_Name" FROM test_states WHERE test_states.area_type = "2"
     and test_states.area_title is not "District of Columbia" GROUP BY test_states.area_title''')
     results = cursor.fetchall()
     test_record = len(results)
     assert test_record == 50
 
-
-def test_save_excel_db():
-    pass
-
-
-def test_table_exist():
-    pass
-
-
-def test_new_table_work():
-    pass
+def test_tables_created_properly(get_db):
+    conn, cursor = get_db
+    sa.setup_db(cursor)
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'school_%';''')  # like does pattern matching with % as the wildcard
+    results = cursor.fetchall()
+    assert len(results) == 1
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'test_%';''')  # like does pattern matching with % as the wildcard
+    results = cursor.fetchall()
+    assert len(results) == 1
